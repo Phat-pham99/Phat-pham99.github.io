ad6164acaeb33cb15f2e7da05b20d7b36b9193bc
phat.pham <phat.pham@terralogic.com>
2023-02-09 17:14:48 +0700

    Updates

--- src/pages/experience/experiencepage.jsx
+++ src/pages/experience/experiencepage.jsx
@@ -1,7 +1,17 @@
 import React,{useState, useEffect, useRef} from 'react';
 import ExperiencePageComponent from "../../components/experience/experiencepage_component.jsx";
-import Language from '@rsuite/icons/legacy/Language';
-import Code from '@rsuite/icons/legacy/Code';
+// import Language from '@rsuite/icons/legacy/Language';
+import Realtime from '@rsuite/icons/legacy/Realtime';
+
+import English from "../../../Assets/england.png";
+import Vietnamese from "../../../Assets/vietnam.png";
+import Japanese from "../../../Assets/japan.png";
+import Django from "../../../Assets/django.svg";
+import React_icon from "../../../Assets/react.svg";
+import Language from "../../../Assets/language.svg";
+import Coding from "../../../Assets/coding.svg";
+import Python from "../../../Assets/python.svg";
+import Javascript from "../../../Assets/javascript.svg";
 import Axios from 'axios';
 import { Collapse } from 'antd';
 import "./experiencepage.css"
@@ -27,36 +37,37 @@ const ExperiencePage = () =>{
         console.log("BRuhhhhh",data_total.languages[0].language);
     };
 
-    const New = () => {
-        return(
-            <></>
-        )
-    };
+    const PeopleFoldSvg = props => (
+        <Python/>
+      );
 
     const Element = () => {
         return(
             <div className='flex flex-row justify-center'>
-        <Collapse className ="text-2xl w-[50%]" defaultActiveKey={['1']}>
-        <Panel header={<div className='flex space-x-3'><Language/> <h1>Language</h1></div>} showArrow={false} key="1"> 
-          <h2>English <a className="text-blue text-base hover:underline" target="_blank" href="https://media.licdn.com/dms/document/C562DAQH2qpDAgJcBuA/profile-treasury-document-pdf-analyzed/0/1657813308703?e=1677110400&v=beta&t=e3mRX8RwxM5Rcmusudoklbkol4rniBIJLRzjXndW08c">Link to certificate</a></h2>
-          <h2>Japanese <a className="text-blue text-base hover:underline" target="_blank" href="https://media.licdn.com/dms/image/C562DAQEgTEMOwzItsw/profile-treasury-image-shrink_1280_1280/0/1635000386719?e=1676527200&v=beta&t=Fjqaud944KbXDYCydYnLE96KR9C-5n4WVc3ervUbK6c">Link to certificate</a></h2>
-          <h2>Vietnamese</h2>
+        <Collapse className ="lg:text-2xl md:text-xl sm:text-base w-[50%]" defaultActiveKey={['1']}>
+        <Panel header={<div className='flex space-x-3'><img className="h-[60px] w-[60px]"src={Language}/><h1>Language</h1></div>} showArrow={false} key="1"> 
+          <h2><div className='flex space-x-[50px]'><img className="h-[70px] w-[70px] align-top"src={English}/><h1>English</h1><a className="text-blue text-base hover:underline" target="_blank" href="https://media.licdn.com/dms/document/C562DAQH2qpDAgJcBuA/profile-treasury-document-pdf-analyzed/0/1657813308703?e=1677110400&v=beta&t=e3mRX8RwxM5Rcmusudoklbkol4rniBIJLRzjXndW08c">Link to certificate</a></div></h2>
+          <h2><div className='flex space-x-[50px]'><img className="h-[60px] w-[60px]"src={Japanese}/><h1>Japanese</h1><a className="text-blue text-base hover:underline" target="_blank" href="https://media.licdn.com/dms/image/C562DAQEgTEMOwzItsw/profile-treasury-image-shrink_1280_1280/0/1635000386719?e=1676527200&v=beta&t=Fjqaud944KbXDYCydYnLE96KR9C-5n4WVc3ervUbK6c">Link to certificate</a></div></h2>
+          <h2><div className='flex space-x-[50px]'><img className="h-[60px] w-[60px]"src={Vietnamese}/><h1>Vietnamese</h1></div></h2>
         </Panel>
-        <Panel header={<div className='flex space-x-3'><Code/> <h1>Skills</h1></div>} showArrow={false} key="2">
-            <Collapse className ="text-2xl">
-            <Panel header="Python" showArrow={false} >
-                <h2>Django</h2>
+        <Panel header={<div className='flex space-x-3'><img className="h-[60px] w-[60px]"src={Coding}/><h1>Skills</h1></div>} showArrow={false} key="2">
+            <Collapse className ="lg:text-2xl md:text-xl sm:text-base">
+            <Panel header={<div className='flex space-x-3'><img className="h-[60px] w-[60px]"src={Python}/><h1>Python</h1></div>} showArrow={false} >
+                <h2><div className='flex space-x-3'><img className="h-[60px] w-[60px]"src={Django}/><h1>Django</h1></div></h2>
                 <h2>Selenium</h2>
-                <Collapse className ="text-2xl">
+                <Collapse className ="lg:text-2xl md:text-xl sm:text-base">
                 <Panel header="Data science - Data Analysis" showArrow={false} >
-                <h2>Stock Forecast</h2>
+                <h2><div className='flex space-x-3'><Realtime/> <h1>TimeSeries Forecast</h1></div></h2>
+                <h2>Natural Language Processing</h2>
+                <h2>Computer Vision</h2>
+                <h2>Data Visualization</h2>
             </Panel>
             </Collapse>
             </Panel>
             </Collapse>
-            <Collapse className ="text-2xl">
-            <Panel header="Javascript" showArrow={false} >
-                <h2>ReactJs</h2>
+            <Collapse className ="lg:text-2xl md:text-xl sm:text-base">
+            <Panel header={<div className='flex space-x-3'><img className="h-[60px] w-[60px]"src={Javascript}/><h1>Javascript</h1></div>} showArrow={false} >
+                <h2>{<div className='flex space-x-3'><img className="h-[60px] w-[60px]"src={React_icon}/><h1>React</h1></div>}</h2>
             </Panel>
         </Collapse>
         </Panel>
@@ -64,6 +75,7 @@ const ExperiencePage = () =>{
           <h2>Terralogic Inc,</h2>
         </Panel>
         </Collapse>
+       
         </div>
         )
     };
