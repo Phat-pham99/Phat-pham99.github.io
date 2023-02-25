e62fc3c6e9ee40f2b703ad385261db95e7a08fbc
Phat-pham99 <hphat99@gmail.com>
2023-02-25 15:07:59 +0700

    projectpage updates

--- src/components/contact/contactpage_component.jsx
+++ src/components/contact/contactpage_component.jsx
@@ -5,11 +5,11 @@ const ContactPageComponent = () =>{
     return (
     <>
         <div className='flex flex-row justify-center'>
-            <h1 className=" text-3xl text-blue-900">Contact Me</h1>
+            <h1 className=" flex flew-row justify-center lg:text-3xl md:text-2xl sm:text-xl">Contact Me</h1>
         </div>
         <br/>
         <div>
-            <h2 className='flex flex-row justify-center text-3xl text-red space-x-1'>Email :  <a className=" hover:underline space-x-1"href="mailto:hphat99@gmail.com?subject=Xin%20chào&body=Tôi%20muốn%20liên%20lạc%20với%20bạn">  Hphat99@gmail.com</a></h2>
+            <h2 className='flex flew-row justify-center lg:text-3xl md:text-xl sm:text-base text-red space-x-1'>Email :  <a className=" hover:underline space-x-1"href="mailto:hphat99@gmail.com?subject=Xin%20chào&body=Tôi%20muốn%20liên%20lạc%20với%20bạn">  Hphat99@gmail.com</a></h2>
         </div>
     </>
     );
