7bf63701beb6de972ee3b62ef134f715fca68b03
phat.pham <57822898+Phat-pham99@users.noreply.github.com>
2023-03-06 15:58:27 +0700

    carousel arrow added

--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -1,17 +1,55 @@
-import React from 'react';
-import "./homepage.css"
+import React, {useRef} from 'react';
 import { Button } from 'antd';
 import FacebookOfficialIcon from '@rsuite/icons/legacy/FacebookOfficial';
 import HomePageComponent from "../../components/homepage/homepage_component.jsx"
 import { Carousel } from 'antd';
-import Phat_blue from "../../../Assets/phat_blue.jpg"
-import Phat_red from "../../../Assets/phat_red.jpg"
-import Phat_yellow from "../../../Assets/phat_yellow.jpg"
+import { LeftSquareOutlined, RightSquareOutlined } from '@ant-design/icons';
+import Phat_blue from "../../../Assets/phat_blue.jpg";
+import Phat_red from "../../../Assets/phat_red.jpg";
+import Phat_yellow from "../../../Assets/phat_yellow.jpg";
+import "./homepage.scss"
 
-const HomePage = () =>{
+const HomePage = () => {
+    const SampleNextArrow = props => {
+        const { className, style, onClick } = props
+        return (
+          <div
+            className={className}
+            style={{
+              ...style,
+              color: 'black',
+              fontSize: '30px',
+              lineHeight: '1.5715',
+            }}
+            onClick={onClick}
+          >
+            <RightSquareOutlined/>
+          </div>
+        )
+      };
+      const SamplePrevArrow = props => {
+        const { className, style, onClick } = props
+        return (
+          <div
+            className={className}
+            style={{
+              ...style,
+              color: 'black',
+              fontSize: '30px',
+              lineHeight: '1.5715'
+            }}
+            onClick={onClick}
+          >
+            <LeftSquareOutlined />
+          </div>
+        )
+      };
+      const settings = {
+        nextArrow: <SampleNextArrow />,
+        prevArrow: <SamplePrevArrow />
+      }
 
     return (
-        // <>
         <div className='body_home min-h-screen h-max  min-w-screen flex flew-col justify-between p-5'>
         <div className="left-component min-h-screen h-max share_butt z-30">
             <Button type="dashed" className="text-black bg-white px-2 fixed">
@@ -23,7 +61,7 @@ const HomePage = () =>{
         <div className="center-component min-h-screen h-max w-[60%] sm:w-[90%] bg-white justify-center space-x-5 rounded-2xl">
             <HomePageComponent/>
       <div className="carousel-container w-[90%] h-[40%] flex flex-row justify-center ">
-        <Carousel autoplay dotPosition={"bottom"} effect={"scrollx"} className="lg:w-[500px] md:w-[400px] sm:w-[200px] h-[40%] flex flex-row justify-center">
+        <Carousel {...settings} arrows autoplay dots={false} effect={"scrollx"} className="lg:w-[500px] md:w-[400px] sm:w-[200px] h-[40%] flex flex-row justify-center">
       <div className='flex flex-row justify-center'>
           <h1 className="p-4 w-full lg:text-xl md:text-lg sm:text-base"><img src={Phat_blue} className="lg:w-[300px] lg:h-[300px] sm: w-[150px] sm:h-[150px]"/>I have bachelor degree
           in Biomedical Engineering but I want to turn my path toward programming.
@@ -42,15 +80,13 @@ const HomePage = () =>{
       </div>
     </Carousel>
     </div>
-            <div className="h-max w-full ">
+            <div className="Comment-section h-max w-full ">
                 <iframe name="f24fa0407cf65b" data-testid="fb:comments Facebook Social Plugin" title="fb:comments Facebook Social Plugin" allowtransparency="false" allowFullScreen={false} scrolling="yes" allow="encrypted-media" src="https://www.facebook.com/v2.5/plugins/comments.php?app_id=726204555155322&amp;channel=https%3A%2F%2Fstaticxx.facebook.com%2Fx%2Fconnect%2Fxd_arbiter%2F%3Fversion%3D46%23cb%3Df26b57f7b013f78%26phat-pham99.github.io%26is_canvas%3Dfalse%26origin%3Dhttps%253A%252F%252Fdinhvanlanh.info%252Ff2f1a85e3fe5e54%26relation%3Dparent.parent&amp;container_width=790&amp;height=100&amp;href=https%3A%2F%2Fphat-pham99.github.io&amp;locale=vi_VN&amp;numposts=5&amp;sdk=joey&amp;version=v2.5" className="min-h-[300px] min-w-[100px] lg:w-full md:w-full sm:w-full px-[5%]"></iframe>
             </div>
         </div>
         <div className="h-[10%]">
-
         </div>
         </div>
-        // </>
     );
 }
 export default HomePage;
