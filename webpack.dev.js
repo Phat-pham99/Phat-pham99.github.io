cb03d4eeeff30c2dc168e8eaaed4599755da7006
phat.pham <57822898+Phat-pham99@users.noreply.github.com>
2023-03-10 16:17:22 +0700

    Updates

--- webpack.dev.js
+++ webpack.dev.js
@@ -80,7 +80,8 @@ module.exports = merge(common, {
             },
             minify: true,
             showErrors: true,
-            template: path.resolve(__dirname, 'src/index.html')
+            template: path.resolve(__dirname, 'src/index.html'),
+            favicon: "./Assets/kaneki.png"
         }),
     ],
     devtool: "cheap-module-source-map",
