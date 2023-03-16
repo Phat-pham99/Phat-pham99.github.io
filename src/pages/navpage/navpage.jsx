8778186b44f5da558026975c5ac7235a15e34ddc
phat.pham <phat.pham@terralogic.com>
2023-03-16 13:43:17 +0700

    Navbar scroll colors

--- src/pages/navpage/navpage.jsx
+++ src/pages/navpage/navpage.jsx
@@ -18,18 +18,18 @@ const NavBar = () =>{
     console.log(scrollPosition);
   },[scrollPosition]);
     return (
-<div className="NavPage-level sticky top-0 z-30 backdrop-filter backdrop-blur-sm hover:backdrop-blur bg-opacity-20 border border-[#c7c7c7] flex flex-row justify-center p-4 w-full ">
-<Navbar className='w-full'>
-    <Nav className='w-[60%] justify-center'>
-        <div className={scrollPosition > 80 ? "NavBar flex flex-row text-green justify-between" : "NavBar flex flex-row text-white justify-between"}>
-            <div className="lg:text-2xl md:text-sm  hover:font-bold"><Nav.Item><Link to="/home">Home</Link></Nav.Item></div>
-            <div className="lg:text-2xl md:text-sm  hover:font-bold"><Nav.Item><Link to="/experience">Experience</Link></Nav.Item></div>
-            <div className="lg:text-2xl md:text-sm  hover:font-bold"><Nav.Item><Link to="/projects">Projects</Link></Nav.Item></div>
-        </div>
-    </Nav>
-</Navbar>
-</div>
-)
+    <div className={scrollPosition > 80 ? "NavPage-level sticky top-0 z-30 backdrop-filter backdrop-blur-sm hover:backdrop-blur bg-opacity-25 flex flex-row justify-center p-4 w-full bg-white text-black" : "NavPage-level sticky top-0 z-30 backdrop-filter backdrop-blur-sm hover:backdrop-blur bg-opacity-20 flex flex-row justify-center p-4 w-full text-white"}>
+    <Navbar className='w-full'>
+        <Nav className='w-[60%] justify-center'>
+            <div className="NavBar flex flex-row justify-between">
+                <div className="lg:text-2xl md:text-sm  hover:font-bold"><Nav.Item><Link to="/home">Home</Link></Nav.Item></div>
+                <div className="lg:text-2xl md:text-sm  hover:font-bold"><Nav.Item><Link to="/experience">Experience</Link></Nav.Item></div>
+                <div className="lg:text-2xl md:text-sm  hover:font-bold"><Nav.Item><Link to="/projects">Projects</Link></Nav.Item></div>
+            </div>
+        </Nav>
+    </Navbar>
+    </div>
+    )
 };
 
 export default NavBar;
