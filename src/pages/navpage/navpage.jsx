8c7da20e4ee03dd7f2e4fa918d92127ce26567fc
HongPhatTL <phat.pham@terralogic.com>
2023-04-18 10:42:58 +0700

    Image shadows + border radius

--- src/pages/navpage/navpage.jsx
+++ src/pages/navpage/navpage.jsx
@@ -22,13 +22,13 @@ export default function NavBar() {
   // },[scrollPosition]);
     return (
     <div className={scrollPosition > 80 ?
-     "NavPage-level sticky top-0 z-30 backdrop-filter backdrop-blur-sm hover:backdrop-blur bg-opacity-20 flex flex-row justify-center p-4 w-full text-black font-Pixel"
-      : "NavPage-level sticky top-0 z-30 backdrop-filter backdrop-blur-sm hover:backdrop-blur bg-opacity-30 flex flex-row justify-center p-4 w-full text-white font-Pixel"}>
+     "NavPage-level sticky top-0 z-30 backdrop-filter backdrop-blur-sm hover:backdrop-blur bg-opacity-20 flex flex-row justify-center p-4 w-full font-Pixel text-black "
+      : "NavPage-level sticky top-0 z-30 backdrop-filter backdrop-blur-sm hover:backdrop-blur bg-opacity-30 flex flex-row justify-center p-4 w-full font-Pixel text-white "}>
     <Navbar className='w-full'>
         <Nav >
             <div className='NavBar w-[70%] flex flex-row justify-center'>
-                <div className="w-[30%]"></div>
-                <div className="NavBar-center w-full flex flex-row lg:justify-between sm:justify-between space-x-3">
+                <div className="w-[50%]"></div>
+                <div className="NavBar-center w-full flex flex-row lg:justify-between sm:justify-between space-x-1">
                     <div className="lg:text-2xl md:text-sm  hover:font-bold"><Nav.Item><Link to="/home">Home</Link></Nav.Item></div>
                     <div className="lg:text-2xl md:text-sm  hover:font-bold"><Nav.Item><Link to="/experience">Experience</Link></Nav.Item></div>
                     <div className="lg:text-2xl md:text-sm  hover:font-bold"><Nav.Item><Link to="/projects">Projects</Link></Nav.Item></div>
