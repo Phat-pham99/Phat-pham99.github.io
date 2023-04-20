322bc2e57a11030fab74206036f9c5179ae34e38
HongPhatTL <phat.pham@terralogic.com>
2023-04-20 10:31:15 +0700

    hashrouter added

--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -54,18 +54,18 @@ export default function HomePage (){
     return (
         <div className='body_home min-h-screen h-max min-w-screen flex flew-col justify-between lg:p-5 sm:p-2'>
         <div className="left-component min-h-screen h-max share_butt z-30 sm:pt-[20px]">
-            <Button type="dashed" className="text-black bg-white lg:px-2 sm:px-0 fixed lg:visible sm:invisible"> 
-                <FacebookOfficialIcon className="lg:h-[22px] lg:w-[22px] sm:h-[15px] sm:w-[15px]" fill="#4267B2"/> <a target="_blank"
-                 href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fphat-pham99.github.io%2F&amp;src=sdkpreparse"
-                 className="fb-xfbml-parse-ignore font-Pixel">  Share </a>
-            </Button>
-            <Button type="" className="bg-white p-[0px] fixed lg:invisible sm:visible">
-                 <a target="_blank"
-                 href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fphat-pham99.github.io%2F&amp;src=sdkpreparse"
-                 className="fb-xfbml-parse-ignore lg:invisible sm:visible"><FacebookOfficialIcon className="sm:h-[30px] sm:w-[30px] lg:invisible sm:visible" fill="#4267B2"/></a>
-            </Button>
+        <a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fphat-pham99.github.io%2F&amp;src=sdkpreparse"
+          className="large-scr-button fb-xfbml-parse-ignore">
+          <Button type="dashed" className="text-black bg-white lg:px-2 sm:px-0 fixed lg:visible sm:invisible font-Pixel"> 
+          <FacebookOfficialIcon className="lg:h-[22px] lg:w-[22px] sm:h-[15px] sm:w-[15px]" fill="#4267B2"/>  Share 
+          </Button></a>
+          <a target="_blank"
+          href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fphat-pham99.github.io%2F&amp;src=sdkpreparse"
+          className="Small-scr-button fb-xfbml-parse-ignore lg:invisible sm:visible"><Button type="" className="bg-white p-[0px] fixed lg:invisible sm:visible">
+          <FacebookOfficialIcon className="sm:h-[30px] sm:w-[30px] lg:invisible sm:visible" fill="#4267B2"/>
+          </Button></a>
         </div>
-        <div className="center-component min-h-screen h-max lg:w-[60%] sm:w-[90%] bg-white justify-center space-x-5 rounded-2xl">
+        <div className="center-component min-h-screen h-max lg:w-[60%] sm:w-[90%] bg-white justify-center space-x-5 rounded-2xl border-[5px] border-green">
             <HomePageComponent/>
       <div className="carousel-container w-[90%] h-[40%] flex flex-row justify-center">
         <Carousel {...settings} arrows autoplay dots={false} effect={"scrollx"} className="lg:w-[500px] md:w-[400px] sm:w-[200px] h-[40%] flex flex-row justify-center">
@@ -77,7 +77,7 @@ export default function HomePage (){
       </div>
       <div className='flex flex-row justify-center font-Pixel'>
       <h1 className="p-4 w-full lg:text-base md:text-lg sm:text-sm"><img src={Phat_red} className="shadow-xl contrast-100 rounded-lg lg:w-[300px] lg:h-[300px] sm: w-[150px] sm:h-[150px]"/>
-      I like to play piano. You can find some of my piano video on my facebook. 
+      I like to play piano. You can find some of <a className='hover:text-green' href='/#/piano'>piano videos</a> on my facebook. 
           </h1>
       </div>
       <div className='flex flex-row justify-center font-Pixel'>
